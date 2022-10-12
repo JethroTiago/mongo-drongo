1b07b07783bf2f4caff4f61ed8447680b30e8042
Jethro Tiago <jethrotiago@hotmail.com>
2022-10-11 21:41:12 -0300

    Added logical commands

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const cartoes = document.querySelectorAll('.cartao-memoria');
 
+let cartaoFoiVirado = false;
+let primeiroCartao, segundoCartao;
+
 function virarCartao() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(!cartaoFoiVirado){
+        cartaoFoiVirado = true;
+        primeiroCartao = this;
+    }
 }
 
 cartoes.forEach(cartao => cartao.addEventListener('click', virarCartao));
